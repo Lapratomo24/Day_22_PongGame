c6b7089acc2df461d4f7e5089c41d5766466997c
Aditya <lapratomo24@yahoo.com>
2024-08-28 17:01:42 +0200

    added a paddle class to create separate paddles

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import turtle
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,27 +7,14 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.goto(x=350, y=0)
-paddle.color("white")
-paddle.penup()
-
-
-def paddle_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def paddle_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(paddle_up, "Up")
-screen.onkey(paddle_down, "Down")
+screen.onkey(r_paddle.paddle_up, "Up")
+screen.onkey(r_paddle.paddle_down, "Down")
+screen.onkey(l_paddle.paddle_up, "w")
+screen.onkey(l_paddle.paddle_down, "s")
 
 game_is_on = True
 
---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.goto(position)
+
+    def paddle_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def paddle_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
